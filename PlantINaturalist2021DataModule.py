2c0b5dd6a696f732d537317f0b340c1695da54e6
Pasoi-Stefan <pasoi0stefan@gmail.com>
2023-01-28 19:33:29 +0200

    Add first model

---
+++ PlantINaturalist2021DataModule.py
@@ -0,0 +1,40 @@
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+
+from torchvision import transforms
+import torchvision.transforms.functional as F
+from torchvision.datasets import ImageFolder
+
+
+class SquarePad:
+    def __call__(self, image):
+        max_wh = max(image.size)
+        p_left, p_top = [(max_wh - s) // 2 for s in image.size]
+        p_right, p_bottom = [max_wh - (s+pad) for s, pad in zip(image.size, [p_left, p_top])]
+        padding = (p_left, p_top, p_right, p_bottom)
+        return F.pad(image, padding, 0, 'constant')
+
+
+class PlantINaturalist2021DataModule(pl.LightningDataModule):
+    def __init__(self, transform, batch_size = 32, num_workers = 0, pin_memory = False , data_dir = "./"):
+        super().__init__()
+        self.transform = transform
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.pin_memory = pin_memory
+        self.data_dir = data_dir
+
+
+    def prepare_data(self):
+        pass
+
+    def setup(self, stage: str):
+        if stage == "fit":
+            self.inaturalist_train = ImageFolder(f"{self.data_dir}/PlantINaturalist2021(2021_train_mini)_250/train", transform=self.transform)
+            self.inaturalist_valid = ImageFolder(f"{self.data_dir}/PlantINaturalist2021(2021_train_mini)_250/validation", transform=self.transform)
+
+    def train_dataloader(self):
+        return DataLoader(self.inaturalist_train, batch_size = self.batch_size, num_workers = self.num_workers, pin_memory = self.pin_memory, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.inaturalist_valid, batch_size = self.batch_size, num_workers = self.num_workers, pin_memory = self.pin_memory)
---
+++ PlantINaturalist2021FinetuneMobileNetv2.py
@@ -0,0 +1,75 @@
+from torchmetrics.functional import accuracy
+import pytorch_lightning as pl
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import torchvision.transforms as transforms
+
+from torchvision.models.mobilenet import mobilenet_v2, MobileNet_V2_Weights
+
+TRANSFORM = transforms.Compose([
+    # transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+TRANSFORM2 = transforms.Compose([
+    transforms.AutoAugment(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+class PlantINaturalist2021FinetuneMobileNetv2(pl.LightningModule):
+    def __init__(self, config):
+        super().__init__()
+        for hyperparamater, value in config.items():
+            setattr(self, hyperparamater, value)
+
+        model = mobilenet_v2(weights=MobileNet_V2_Weights.IMAGENET1K_V2)
+        for param in model.parameters():
+            param.requires_grad = False
+
+        trainable_layers = list(model.features.children())[-self.num_trainable_layers:]
+    
+        for layer in trainable_layers:
+            for param in layer.parameters():
+                param.requires_grad = True
+
+        # by default trainable
+        model.classifier = nn.Sequential(
+            nn.Dropout(p=0.2),
+            nn.Linear(model.last_channel, self.num_classes),
+        )
+
+        self.model = model
+
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.model(x)
+        loss = F.cross_entropy(y_hat, y)
+
+        acc = accuracy(y_hat, y, task="multiclass", num_classes=self.num_classes)
+        metrics = {"train_acc": acc, "train_loss": loss}
+        self.log_dict(metrics, on_step=True, on_epoch=True, prog_bar = True)
+        return loss
+
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.model(x)
+        loss = F.cross_entropy(y_hat, y)
+
+        acc = accuracy(y_hat, y, task="multiclass", num_classes=self.num_classes)
+        metrics = {"val_acc": acc, "val_loss": loss}
+        self.log_dict(metrics, on_step=True, on_epoch=True, prog_bar = True)
+        return metrics
+
+    def configure_optimizers(self):
+        # optimizer = torch.optim.SGD(self.model.classifier.parameters(), lr=self.learning_rate, momentum=0.9)
+        # lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+        # return [optimizer], [lr_scheduler]
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=self.lr_decay_epoch_step_size, gamma=self.lr_decay_rate)
+        return [optimizer], [lr_scheduler]
